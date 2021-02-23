975ec17e4cdc8b219ae315ab12bc98d71074f9c1
natagetup <nata.getup@gmail.com>
2021-02-23 12:13:44 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strLen = str.length;
+
+  for (let [start, end] of bracketsConfig) {
+    str = str.split(`${start}${end}`).join('');
+  }
+
+  if (str.length === 0) {
+    return true;
+  } else if (str.length === strLen) {
+    return false;
+  }
+
+  return check(str, bracketsConfig);
 }
